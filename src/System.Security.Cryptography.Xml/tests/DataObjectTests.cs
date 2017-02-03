865b0601e5864e906e5331e4bc9d89a1506455c5
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-02-03 11:24:45 +1100

    Starting equality comparers

--- src/System.Security.Cryptography.Xml/tests/DataObjectTests.cs
+++ src/System.Security.Cryptography.Xml/tests/DataObjectTests.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
+using Windows.Data.Xml.Dom;
 using Xunit;
+using XmlDocument = System.Xml.XmlDocument;
+using XmlElement = System.Xml.XmlElement;
 
 namespace System.Security.Cryptography.Xml.Tests
 {
@@ -16,6 +18,28 @@ namespace System.Security.Cryptography.Xml.Tests
             DataObject dataObject = new DataObject();
 
             Assert.Empty(dataObject.Data);
+            Assert.Null(dataObject.Encoding);
+            Assert.Null(dataObject.Id);
+            Assert.Null(dataObject.MimeType);
+
+            XmlElement xmlElement = dataObject.GetXml();
+            Assert.NotNull(xmlElement);
+            Assert.Equal("<Object xmlns=\"http://www.w3.org/2000/09/xmldsig#\" />", xmlElement.OuterXml);
+        }
+
+        [Fact]
+        public void Constructor_Params()
+        {
+            string id = "id";
+            string mimeType = "text/test";
+            string encoding = "utf-42";
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement initialXmlElement = xmlDocument.CreateElement("name");
+            DataObject dataObject = new DataObject(id, mimeType, encoding, initialXmlElement);
+
+            Assert.Collection(
+                (IEnumerable<IXmlNode>) dataObject.Data, 
+                element1 => Assert.Equal(element1, initialXmlElement));
             Assert.Null(dataObject.Encoding);
             Assert.Null(dataObject.Id);
             Assert.Null(dataObject.MimeType);
---
+++ src/System.Security.Cryptography.Xml/tests/XmlNodeListEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace System.Security.Cryptography.Xml.Tests
+{
+    public class XmlNodeListEqualityComparer: IEqualityComparer<XmlNodeList>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Equals(XmlNodeList x, XmlNodeList y)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int GetHashCode(XmlNodeList obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
